736166f123f1311f5493533053e517515b13ebd3
John Parejko <parejkoj@uw.edu>
2021-12-28 15:06:54 -0800

    Remove validation_data references
    These configs and scripts are for the now deprecated validation_data packages,
    running with the deprecated validate_drp.

--- config/cfhtConfig.py
+++
@@ -1,7 +0,0 @@
-# We do not have transmission curves attached to our validation repos yet
-config.processCcd.isr.doAttachTransmissionCurve = False
-# Run meas_modelfit to compute CModel fluxes
-import lsst.meas.modelfit
-config.processCcd.calibrate.measurement.plugins.names |= [
-            "modelfit_DoubleShapeletPsfApprox", "modelfit_CModel"]
-config.processCcd.calibrate.measurement.slots.modelFlux = 'modelfit_CModel'
